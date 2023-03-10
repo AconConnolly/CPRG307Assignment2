98609d27eeb3f29f14f8cf3d8bffe26f7c931386
Miguel Mulingbayan <122918015+MiguelMwithTheM@users.noreply.github.com>
2023-03-10 15:57:49 -0700

    Add files via upload

--- assignment2body.sql
+++ assignment2body.sql
@@ -1,9 +1,40 @@
 --==MainCode==--
 DECLARE
 
+--Constants
+    k_tDebit CHAR(1) := 'D';
+    k_tCredit CHAR(1) := 'C';
+
+--Cursor
+    CURSOR cur_transData IS
+    SELECT *
+    FROM NEW_TRANSACTIONS;
+
+--Exceptions
+    ex_invalidAccNum_update EXCEPTION;
+    ex_nVal_update EXCEPTION;
+    ex_invalidTransType_update EXCEPTION;
+    ex_missingTransNum_update EXCEPTION;
+
 BEGIN
 
+    FOR rec_transData IN cur_transData LOOP
+
+        DBMS_OUTPUT.PUT_LINE(rec_transData.Account_no);
+
+    END LOOP;
+
+/*
 EXCEPTION
+    WHEN ex_invalidAccNum_update THEN
+        RAISE_APPLICATION_ERROR(-20031, 'Account # does not exist');
+    WHEN ex_nVal_update THEN
+        RAISE_APPLICATION_ERROR(-20032, 'Negative values are invalid');
+    WHEN ex_invalidTransType_update THEN
+        RAISE_APPLICATION_ERROR(-20033, 'Invalid trasaction type');
+    WHEN ex_missingTransNum_update THEN
+        RAISE_APPLICATION_ERROR(-20034, 'Missing transaction number');
+*/
 
 END;
 /
