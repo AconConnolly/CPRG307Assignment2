33e49de4f323ec72f302b1d6a36d4ff1ddc1be97
Miguel Mulingbayan <122918015+MiguelMwithTheM@users.noreply.github.com>
2023-03-13 11:16:40 -0600

    Add files via upload

--- assignment2body.sql
+++ assignment2body.sql
@@ -10,17 +10,32 @@ DECLARE
     SELECT *
     FROM NEW_TRANSACTIONS;
 
+
+
 --Exceptions
     ex_invalidAccNum_update EXCEPTION;
     ex_nVal_update EXCEPTION;
     ex_invalidTransType_update EXCEPTION;
-    ex_missingTransNum_update EXCEPTION;
+    ex_missingTransNum_update EXCEPTION;uyhhgggbvvvvvvvvvvvvvvvvf
 
 BEGIN
 
     FOR rec_transData IN cur_transData LOOP
 
-        DBMS_OUTPUT.PUT_LINE(rec_transData.Account_no);
+        --DBMS_OUTPUT.PUT_LINE(rec_transData.Account_no);
+
+        DECLARE
+        --New Transaction Variablaes (For Ease)
+            v_ntTransNo NEW_TRANSACTIONS.Transaction_no%TYPE;
+            v_ntTransDate NEW_TRANSACTIONS.Transaction_date%TYPE;
+            v_ntDesc NEW_TRANSACTIONS.Description%TYPE;
+            v_ntAccNo NEW_TRANSACTIONS.Account_no%TYPE;
+            v_ntTransType NEW_TRANSACTIONS.Transaction_type%TYPE;
+            v_ntTransAmount NEW_TRANSACTIONS.Transaction_amount%TYPE;
+
+        BEGIN
+
+        END;
 
     END LOOP;
 
