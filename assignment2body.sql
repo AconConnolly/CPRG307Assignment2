9a7419e388d56a9c07593c70c1253237c9b96336
Miguel Mulingbayan <122918015+MiguelMwithTheM@users.noreply.github.com>
2023-03-13 13:28:39 -0600

    Add files via upload

--- assignment2body.sql
+++ assignment2body.sql
@@ -6,37 +6,52 @@ DECLARE
     k_tCredit CHAR(1) := 'C';
 
 --Cursor
-    CURSOR cur_transData IS
+    CURSOR cur_ntData IS
     SELECT *
     FROM NEW_TRANSACTIONS;
 
+    Cursor cur_accData IS
+    SELECT *
+    FROM ACCOUNT;
+/*
+--New Transaction Variablaes (For Ease)
+    v_ntTransNo NEW_TRANSACTIONS.Transaction_no%TYPE;
+    v_ntTransDate NEW_TRANSACTIONS.Transaction_date%TYPE;
+    v_ntDesc NEW_TRANSACTIONS.Description%TYPE;
+    v_ntAccNo NEW_TRANSACTIONS.Account_no%TYPE;
+    v_ntTransType NEW_TRANSACTIONS.Transaction_type%TYPE;
+    v_ntTransAmount NEW_TRANSACTIONS.Transaction_amount%TYPE;
+*/
 
+/*
+--Account Variables (For Ease)
+    v_aBal ACCOUNT.Account_balance%TYPE;
+*/
+
+    v_accCount NUMBER(3) := 0;
 
 --Exceptions
     ex_invalidAccNum_update EXCEPTION;
     ex_nVal_update EXCEPTION;
     ex_invalidTransType_update EXCEPTION;
-    ex_missingTransNum_update EXCEPTION;uyhhgggbvvvvvvvvvvvvvvvvf
+    ex_missingTransNum_update EXCEPTION;
 
 BEGIN
 
-    FOR rec_transData IN cur_transData LOOP
+    FOR rec_accData IN cur_accData LOOP
+        v_accCount := 0;
 
-        --DBMS_OUTPUT.PUT_LINE(rec_transData.Account_no);
+        FOR rec_ntData IN cur_ntData LOOP
 
-        DECLARE
-        --New Transaction Variablaes (For Ease)
-            v_ntTransNo NEW_TRANSACTIONS.Transaction_no%TYPE;
-            v_ntTransDate NEW_TRANSACTIONS.Transaction_date%TYPE;
-            v_ntDesc NEW_TRANSACTIONS.Description%TYPE;
-            v_ntAccNo NEW_TRANSACTIONS.Account_no%TYPE;
-            v_ntTransType NEW_TRANSACTIONS.Transaction_type%TYPE;
-            v_ntTransAmount NEW_TRANSACTIONS.Transaction_amount%TYPE;
+            IF (rec_ntData.Account_no = rec_accData.Account_no) THEN
+                v_accCount := v_accCount + 1;
+            END IF;
 
-        BEGIN
+        END LOOP;
 
-        END;
-
+        DBMS_OUTPUT.PUT_LINE(rec_accData.Account_no);
+        DBMS_OUTPUT.PUT_LINE(v_accCount);
+        
     END LOOP;
 
 /*
