1e1fff72ee727e07e663e57779368cfe07e1cc5d
Miguel Mulingbayan <122918015+MiguelMwithTheM@users.noreply.github.com>
2023-03-16 11:50:17 -0600

    Add files via upload

--- assgn2NewSkeleton.sql
+++ assgn2NewSkeleton.sql
@@ -48,10 +48,43 @@ DECLARE
     SELECT *
     FROM NEW_TRANSACTIONS;
 
+    --Variables
+    v_accAccNo ACCOUNT.Account_no%TYPE; --The Account_no in the ACCOUNT TABLE 
+    v_accAccTypeCode ACCOUNT.Account_type_code%TYPE; --The Account_type_code in the ACCOUNT TABLE
+    v_accAccBal ACCOUNT.Account_balance%TYPE; --The Account_balance in the ACCOUNT TABLE
+    v_atDefTransType ACCOUNT_TYPE.Default_trans_type%TYPE; --The Default_trans_type in the ACCOUNT_ TABLE
+
 BEGIN
     FOR rec_ntData IN cur_ntData LOOP
 
-    DBMS_OUTPUT.PUT_LINE(rec_ntData.Transaction_type);
+        SELECT Account_no, Account_type_code, Account_balance
+        INTO v_accAccNo, v_accAccTypeCode, v_accAccBal
+        FROM ACCOUNT
+        WHERE Account_no = rec_ntData.Account_no;
+        
+        SELECT Default_trans_type
+        INTO v_atDefTransType
+        FROM ACCOUNT_TYPE
+        WHERE Account_type_code = v_accAccTypeCode;
+
+        IF (rec_ntData.Account_no = v_accAccNo) THEN
+            CASE
+                WHEN (v_atDefTransType = rec_ntData.Transaction_type) THEN
+                    UPDATE ACCOUNT
+                    SET Account_balance = Account_balance + rec_ntData.Transaction_amount;
+
+                WHEN (v_atDefTransType != rec_ntData.Transaction_type) THEN
+                    UPDATE ACCOUNT
+                    SET Account_balance = Account_balance - rec_ntData.Transaction_amount;
+                    
+                ELSE NULL;
+            END CASE;
+
+        END IF;
+
+        DBMS_OUTPUT.PUT_LINE('--------------');
+        DBMS_OUTPUT.PUT_LINE(rec_ntData.Account_no);
+        DBMS_OUTPUT.PUT_LINE(v_atDefTransType);
 
     END LOOP;
 
