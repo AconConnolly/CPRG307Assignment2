ed9ee79b4157e9bc233aed67cea90efc9e1d9480
Miguel Mulingbayan <122918015+MiguelMwithTheM@users.noreply.github.com>
2023-03-16 11:40:28 -0600

    Add files via upload

--- assgn2NewSkeleton.sql
+++ assgn2NewSkeleton.sql
@@ -1,8 +1,21 @@
+SET LINESIZE 60
+SET PAGESIZE 66
+SET SERVEROUTPUT ON
+
+--==Constants
+    k_tDebit CHAR(1) := 'D';
+    k_tCredit CHAR(1) := 'C';
+
+--==NEW_TRANSACTIONS
+CURSOR cur_ntData IS
+SELECT *
+FROM NEW_TRANSACTIONS;
+
 --==Get Account_no for Account_Bal (Where statement for update)
 --Dec
-v_accAccNo ACCOUNT.Account_no%TYPE;
-v_accAccTypeCode ACCOUNT.Account_type_code%TYPE;
-v_accAccBal ACCOUNT.Account_balance%TYPE;
+v_accAccNo ACCOUNT.Account_no%TYPE; --The Account_no in the ACCOUNT TABLE 
+v_accAccTypeCode ACCOUNT.Account_type_code%TYPE; --The Account_type_code in the ACCOUNT TABLE
+v_accAccBal ACCOUNT.Account_balance%TYPE; --The Account_balance in the ACCOUNT TABLE
 
 --Beg
 SELECT Account_no, Account_type_code, Account_balance
@@ -14,7 +27,7 @@ WHERE Account_no = rec_ntData.Account_no;
 
 --==Get Default_trans_type for comparison (D or C)
 --Dec
-v_atDefTransType ACCOUNT_TYPE.Default_trans_type%TYPE;
+v_atDefTransType ACCOUNT_TYPE.Default_trans_type%TYPE; --The Default_trans_type in the ACCOUNT_ TABLE
 
 --Beg
 SELECT Default_trans_type
@@ -24,3 +37,23 @@ WHERE Account_type_code = v_accAccTypeCode;
 
 --End
 
+--==TestCode
+DECLARE
+    --Constants
+    k_tDebit CHAR(1) := 'D';
+    k_tCredit CHAR(1) := 'C';
+
+    --Cursor
+    CURSOR cur_ntData IS
+    SELECT *
+    FROM NEW_TRANSACTIONS;
+
+BEGIN
+    FOR rec_ntData IN cur_ntData LOOP
+
+    DBMS_OUTPUT.PUT_LINE(rec_ntData.Transaction_type);
+
+    END LOOP;
+
+END;
+/
